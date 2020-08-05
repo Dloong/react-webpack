e091aaf832da06092217fac4fe01daf55cca87a6
duanshilong <shilong.duan@silot.ai>
2020-08-05 10:03:18 +0800

    init

---
+++ src/views/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from "../images/logo.jpg"
+
+export default function Content () {
+    return (
+        <div>
+            <img src={Logo}></img>
+        </div>
+    )
+}
---
+++ src/views/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./header.scss"
+function Header() {
+    return (
+        <div className={'header_wrap'}>这个是头部组件</div>
+    )
+}
+
+export default Header
